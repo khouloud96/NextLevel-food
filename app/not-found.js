cc214e7cc00892a671c44ef913006f1b15682f17
Khouloud Ben Salem <khouloud.bensalem@ensi-uma.tn>
2024-09-04 09:59:19 +0200

    handling errors and not found states

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return(
+        <main className="not-found">
+            <h1>Not found</h1>
+            <p>Unfortnuately, we could not find the requested page or reource.</p>
+        </main>
+    );
+}
